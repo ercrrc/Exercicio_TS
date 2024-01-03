f991c810b93ebd3efdd32233dee461b9ddf87331
ercrrc <134560333+ercrrc@users.noreply.github.com>
2024-01-03 19:22:59 -0300

    refazendo

--- src/exercicio.ts
+++ src/exercicio.ts
@@ -1,3 +1,10 @@
+function calculaArea(base:  number, altura: number): number {
+    return base * altura;
+}
+
+console.log( calculaArea(5, 6));
+
+
 function ola(nome: string,): string {
     return "ola" + nome +"!";
 }
